b0fd965bc09ab5239a839a1d06ceaec3524b2ae8
Bandaru pavan kumar <pbandarubtech24@ced.alliance.edu.in>
2025-12-14 17:10:02 +0530

    Fix blank page on Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/lifelens-ai/', // <-- must match your repo name
+  base: '/',   // ✅ ADD THIS LINE
   plugins: [react()]
 })
